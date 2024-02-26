0a6c824809663bc1791cae807db4fadd95eac1a8
rojhat <rojhatbirel43821@gmail.com>
2024-02-27 01:10:26 +0300

    Kod değişiklikleri eklendi

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let addBtn = document.getElementById('add');
 let taskInput = document.getElementById('input');
 let clearBtn = document.getElementById('clear');
+
+
 let taskList = [];
-
-
-addBtn.addEventListener('click', () => {
+function addTask() {
     let task = taskInput.value;
     if (task) {
         taskList.push(task);
@@ -16,25 +16,33 @@ addBtn.addEventListener('click', () => {
                     <input class="chbx" type="checkbox" id="${taskList.length}">
                     <label for="${taskList.length}">${task}</label>
                 </div>
-                <img class="img1" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAs0lEQVR4nO3VSw6CMBRG4Y5kEW5RXKaPEW7F4PyYGzpwYFUSTIueL2lCoPy59EFTkiRJkiRJqwB0wB44Abfc4rqPZ6vIALbAhbIh+jSdwTQCr15+DOmazWCaxk/tCkXUzwDOMwKOhSLqZwDjjICxUET9DOYFXBco4jsZ/NDS6hfYqPUzmH578Ut7J/psCkU0kZHyQTQscJhVz0jxlTFlsf7yxot2yPeej0CjGZIkSZKkf3QHsYLvosSx6nEAAAAASUVORK5CYII=">
-            </div>
+                <span id="iconspan"> <svg class="img1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" id="Ellipsis"><path d="M576 736v192q0 40-28 68t-68 28H288q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68zm512 0v192q0 40-28 68t-68 28H800q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68zm512 0v192q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68z" fill="#ffffff" class="color000000 svgShape"></path></svg></span>
         </li>`;
         ul.insertAdjacentHTML('afterbegin', li);
+        let settBtn = document.querySelector('#iconspan');
+        settBtn.addEventListener('click', function () {
+            console.log('tıklandı');
+        });
     }
     taskInput.value = '';
     console.log(taskList);
-});
+}
+
+addBtn.addEventListener('click', addTask);
+
 taskInput.addEventListener("keydown", function (event) {
     if(event.key === "Enter"){
-        addBtn.click();
+        addTask();
     }
     
 })
 
+function iconClick() {
+    console.log('clicked');
+    
+}
 clearBtn.addEventListener('click', () => {
     let ul = document.getElementById('list');
     ul.innerHTML = '';
     taskList = [];
 }  );
-
-
