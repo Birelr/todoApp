8e9ba7dade034b57d5f1793ee0f039c795482337
rojhat <rojhatbirel43821@gmail.com>
2024-02-27 11:36:16 +0300

    Düzenleme düğmesine tıklandığında görevi düzenleme işlevi eklendi

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function addTask() {
                 <svg class="img1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" id="Ellipsis"><path d="M576 736v192q0 40-28 68t-68 28H288q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68zm512 0v192q0 40-28 68t-68 28H800q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68zm512 0v192q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68z" fill="#ffffff" class="color000000 svgShape"></path></svg>
                 <div id="dropdown" class="dropdown-content">
                     <div id="deletebtn" onclick="deleteTask()"><i class="fa-regular fa-trash-can"></i> Delete</div>
-                    <div id="editbtn"><i class="fa-regular fa-pen-to-square"></i> Edit</div>
+                    <div id="editbtn" onclick="editTask()"><i class="fa-regular fa-pen-to-square"></i> Edit</div>
                 </div>
                 </span>
         </li>`;
@@ -34,11 +34,7 @@ function addTask() {
                 dropdownContent.style.display = "none";
             }
         });
-        let deletebtn = document.getElementById('deletebtn');
-        let editbtn = document.getElementById('editbtn');
-        
-        
-        
+
 
     }
     taskInput.value = '';
@@ -79,4 +75,10 @@ clearBtn.addEventListener('click', () => {
 function deleteTask() {
     let li = document.querySelector('.lielement');
     li.remove();
-}
+};
+function editTask() {
+    let li = document.querySelector('.lielement');
+    let labelElement = li.querySelector('label');
+    li.remove();
+    taskInput.value = labelElement.textContent;
+};
