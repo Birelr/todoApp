6175781dcd2e05060608343a595c8bf0857a78bb
rojhat <rojhatbirel43821@gmail.com>
2024-03-02 00:43:47 +0300

    "To-Do List App başlığı güncellendi ve bazı stil düzenlemeleri yapıldı."

--- script.js
+++ script.js
@@ -1,105 +1,74 @@
-let addBtn = document.getElementById('add');
-let taskInput = document.getElementById('input');
-let clearBtn = document.getElementById('clear');
+const addBtn = document.getElementById('add');
+const taskInput = document.getElementById('input');
+const clearBtn = document.getElementById('clear');
+const list = document.getElementById('list');
 
-let taskList = [];
+let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+
 function addTask() {
-    let task = taskInput.value;
+    let task = taskInput.value.trim(); 
     if (task) {
         taskList.push(task);
-        let ul = document.getElementById('list');
-        let li = `
-        <li class="lielement">
-            <div class="formcheck">
-                <div class="taskl">
-                    <input class="chbx" type="checkbox" id="${taskList.length}">
-                        <label for="${taskList.length}">${task}</label>
-                </div>
-                <span id="iconspan"> 
-                <svg class="img1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" id="Ellipsis"><path d="M576 736v192q0 40-28 68t-68 28H288q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68zm512 0v192q0 40-28 68t-68 28H800q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68zm512 0v192q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68z" fill="#ffffff" class="color000000 svgShape"></path></svg>
-                <div id="dropdown" class="dropdown-content">
-                    <div id="deletebtn" onclick="deleteTask()"><i class="fa-regular fa-trash-can"></i> Delete</div>
-                    <div id="editbtn" onclick="editTask()"><i class="fa-regular fa-pen-to-square"></i> Edit</div>
-                </div>
-                </span>
-        </li>`;
-        ul.insertAdjacentHTML('afterbegin', li);
-        let settBtn = document.querySelector('#iconspan');
-        let dropdownContent = document.getElementById("dropdown");
-        settBtn.addEventListener('click', function () {
-            if (dropdownContent.style.display === "none") {
-                dropdownContent.style.display = "block";
-            } else {
-                dropdownContent.style.display = "none";
-            }
-        });
+        updateTaskList();
     }
     taskInput.value = '';
-    document.addEventListener('change', function(event) {
-        if (event.target.classList.contains('chbx')) {
-            let labelForCheckbox = event.target.nextElementSibling;
-            if (event.target.checked) {
-                labelForCheckbox.classList.add('checked');
-            } else {
-                labelForCheckbox.classList.remove('checked');
-            }
-        }
-    });
-
-    
-    document.addEventListener('click', function(event) {
-        let dropdowns = document.querySelectorAll('.dropdown-content');
-        dropdowns.forEach(function(dropdownContent) {
-            let settBtn = dropdownContent.previousElementSibling;
-            let isClickInside = settBtn.contains(event.target);
-    
-            if (!isClickInside) {
-                dropdownContent.style.display = "none";
-            }
-        });
-    });
-    
-    
 }
 
+function updateTaskList() {
+    list.innerHTML = ''; 
+
+    taskList.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.classList.add('lielement'); 
+
+        li.innerHTML = `
+            <div class="formcheck">
+                <div class="taskl">
+                    <input class="chbx" type="checkbox" id="task_${index}">
+                    <label for="task_${index}">${task}</label>
+                </div>
+                <span id="iconspan">
+                <svg class="img1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" id="Ellipsis"><path d="M576 736v192q0 40-28 68t-68 28H288q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68zm512 0v192q0 40-28 68t-68 28H800q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68zm512 0v192q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68V736q0-40 28-68t68-28h192q40 0 68 28t28 68z" fill="#ffffff" class="color000000 svgShape"></path></svg>
+                <div id="dropdown" class="dropdown-content">
+                    <div id="deletebtn" onclick="deleteTask(${index})"><i class="fa-regular fa-trash-can"></i> Delete</div>
+                    <div id="editbtn" onclick="editTask(${index})"><i class="fa-regular fa-pen-to-square"></i> Edit</div>
+                </div>
+                </span>
+            </div>
+        `;
+        const settBtn = li.querySelector('#iconspan');
+        const dropdownContent = li.querySelector('#dropdown');
+        settBtn.addEventListener('click', () => { 
+            dropdownContent.style.display = dropdownContent.style.display === "none" ? "block" : "none";
+        });
+
+        list.appendChild(li);
+    });
+
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+}
+
+function deleteTask(index) {
+    taskList.splice(index, 1);
+    updateTaskList();
+}
+function editTask(index) {
+    let task = taskList[index];
+    taskInput.value = task;
+    taskList.splice(index, 1);
+    updateTaskList();
+}
+
+clearBtn.addEventListener('click', () => {
+    taskList = []; 
+    updateTaskList();
+});
+document.addEventListener('DOMContentLoaded', updateTaskList); 
+document.addEventListener('click', (event) => {
+});
 addBtn.addEventListener('click', addTask);
-taskInput.addEventListener("keydown", function (event) {
-    if(event.key === "Enter"){
+taskInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
         addTask();
     }
-    
-})
-
-function iconClick() {
-    console.log('clicked');
-    
-}
-clearBtn.addEventListener('click', () => {
-    let ul = document.getElementById('list');
-    ul.innerHTML = '';
-    taskList = [];
-}  );
-
-function deleteTask() {
-    let li = document.querySelector('.lielement');
-    let labelElement = li.querySelector('label');
-    let task = labelElement.textContent;
-    let index = taskList.indexOf(task);
-    if (index > -1) {
-        taskList.splice(index, 1);
-    }
-    li.remove();
-};
-function editTask() {
-    let li = document.querySelector('.lielement');
-    let labelElement = li.querySelector('label');
-    let task = labelElement.textContent;
-    let index = taskList.indexOf(task);
-    if (index > -1) {
-        taskList.splice(index, 1);
-    }
-    li.remove();
-    taskInput.value = task;
-};
-
-
+}); 
